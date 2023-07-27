05eca6985d33e41dc69ba5f817e824af59423173
ma-molinari <ma.molinari@hotmail.com>
2023-07-27 19:42:24 -0300

    feat: use tailwind-merge in components

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,20 @@
+import { twMerge } from "tailwind-merge";
+
 interface Props {
   label: string;
   type?: "button" | "submit";
+  className?: string;
   onClick?: () => void;
 }
 
-const Button = ({ label, type = "button", onClick }: Props) => {
+const Button = ({ label, type = "button", className, onClick }: Props) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="text-white bg-blue-600 rounded hover:bg-blue-700 h-11"
+      className={twMerge(
+        `text-white bg-blue-600 rounded hover:bg-blue-700 h-11 ${className}`
+      )}
     >
       {label}
     </button>
--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,10 +1,12 @@
 import { UseFormRegister } from "react-hook-form";
+import { twMerge } from "tailwind-merge";
 
 interface Props {
   name: string;
   placeholder: string;
   type?: "text" | "password";
   message?: string;
+  className?: string;
   register: UseFormRegister<any>;
 }
 
@@ -13,12 +15,15 @@ const TextField = ({
   placeholder,
   type = "text",
   message,
+  className,
   register,
 }: Props) => {
   return (
     <div>
       <input
-        className="w-full border-2 border-gray-200 border-solid rounded h-14 indent-4 focus-visible:border-blue-600"
+        className={twMerge(
+          `w-full border-2 border-gray-200 border-solid rounded h-14 indent-4 focus-visible:border-blue-600 ${className}`
+        )}
         placeholder={placeholder}
         type={type}
         {...register?.(name)}
