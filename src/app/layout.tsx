ce905efee6865a187139d5ba80261543637ec32d
ma-molinari <ma.molinari@hotmail.com>
2023-09-04 20:03:31 -0300

    -m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 
 import Providers from "@global-libs/react-query";
 import "./globals.css";
+import { Toaster } from "@global-components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>{children}</Providers>
+        <Toaster />
       </body>
     </html>
   );
