3bdd0a43f2fcdc0691e09e2166bf4971b3151162
ma-molinari <ma.molinari@hotmail.com>
2023-10-18 19:43:02 -0300

    -m

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import HomeScreen from "@modules/home/screens/Main";
+
+export const metadata = {
+  title: "Home - M-Feed",
+  description: "Browse your feed, see your friends posts and your own.",
+};
+
+const Home = () => {
+  return <HomeScreen />;
+};
+
+export default Home;
---
+++ src/modules/home/screens/Main/index.tsx
@@ -0,0 +1,11 @@
+import FeedContainer from "@modules/home/components/FeedContainer";
+
+const HomeScreen = () => {
+  return (
+    <main className="h-full py-6 overflow-auto">
+      <FeedContainer />
+    </main>
+  );
+};
+
+export default HomeScreen;
