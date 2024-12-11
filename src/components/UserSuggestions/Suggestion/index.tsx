c18c83768f2e4ec4fe8ed217314bde7a9ad893c7
ma-molinari <ma.molinari@hotmail.com>
2024-12-11 13:03:34 -0300

    feat: follow unfollow

---
+++ src/components/UserSuggestions/Suggestion/index.tsx
@@ -0,0 +1,33 @@
+import { User } from "@entities/user";
+import HoverUser from "@global-components/HoverUser";
+import { Button } from "@global-components/ui/button";
+import UserCard from "@global-components/ui/user-card";
+import useFollowHandler from "@global-hooks/useFollowHandler";
+
+interface Props {
+  data: User;
+}
+
+const Suggestion = ({ data }: Props) => {
+  const { onHandleFollow } = useFollowHandler(data.id);
+
+  return (
+    <div className="flex items-center gap-3">
+      <HoverUser data={data}>
+        <div>
+          <UserCard data={data} variant="small" />
+        </div>
+      </HoverUser>
+      <Button
+        size="sm"
+        variant="outline"
+        className="ml-auto"
+        onClick={onHandleFollow}
+      >
+        Follow
+      </Button>
+    </div>
+  );
+};
+
+export default Suggestion;
--- src/components/UserSuggestions/index.tsx
+++ src/components/UserSuggestions/index.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import { memo } from "react";
 import { useGetUserSuggestions } from "@services/users";
-import { Button } from "@global-components/ui/button";
-import HoverUser from "@global-components/HoverUser";
 import UserSuggestionsLoading from "./UserSuggestionsLoading";
-import UserCard from "@global-components/ui/user-card";
+import Suggestion from "./Suggestion";
 
 const UserSuggestions = () => {
   const { data: suggestions, isLoading } = useGetUserSuggestions();
@@ -19,20 +16,11 @@ const UserSuggestions = () => {
       <h3 className="font-semibold">Users to follow</h3>
       <div className="flex flex-col mt-4 gap-y-4">
         {suggestions?.map((item) => (
-          <div key={item.id} className="flex items-center gap-3">
-            <HoverUser data={item}>
-              <div>
-                <UserCard data={item} variant="small" />
-              </div>
-            </HoverUser>
-            <Button size="sm" variant="outline" className="ml-auto">
-              Follow
-            </Button>
-          </div>
+          <Suggestion key={item.id} data={item} />
         ))}
       </div>
     </div>
   );
 };
 
-export default memo(UserSuggestions);
+export default UserSuggestions;
